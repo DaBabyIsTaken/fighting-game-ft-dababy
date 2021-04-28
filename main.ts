342066704540f0fb837904dd0a981ad3ee6ebfe0
Eshaan Lal <83054662+DaBabyIsTaken@users.noreply.github.com>
2021-04-28 14:08:14 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . . 8 5 5 8 5 5 8 . . . . 
+    . . . . . 8 5 5 8 5 5 8 . . . . 
+    . . . . . 8 5 5 8 5 5 8 8 8 8 . 
+    . . . . . 8 8 f f f f f 8 8 8 . 
+    . . . . . f f f f f f f f f . . 
+    . . . . f f e e e e e e e f f . 
+    . . . . f f e f f e f f e f f . 
+    . . . . f f e 1 f e f 1 e f f . 
+    . . . . f f e e e e e e e f f . 
+    . . . . . f e e 3 e e e f f . . 
+    . . . . . f f e 3 3 3 f f . . . 
+    . . . . . . f f e e f f . . . . 
+    . . . f f f f f f f f f f f f . 
+    . . . f 1 1 f . . . . f 1 1 f . 
+    . . . f 1 1 f . . . . f 1 1 f . 
+    . . . f f f f . . . . f f f f . 
+    `, SpriteKind.Player)
